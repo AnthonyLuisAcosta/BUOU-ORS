3db51f1af2728a40b11d42374160c38423ea1a43
Anthony Luis Acosta <anthonyluislirios.acosta@bicol-u.edu.ph>
2022-08-03 11:28:35 +0800

    Layout and Nav Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -545,7 +545,7 @@
         @if (Route::has('login'))
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             @auth
-            <a href="{{ url('dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 " style="">Dashboard</a>
+            <a href="{{ url('dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 ">Dashboard</a>
             @else
             <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 ">Log in</a>
 
