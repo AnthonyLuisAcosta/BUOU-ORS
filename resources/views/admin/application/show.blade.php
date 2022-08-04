3181027cd99b7514c7416e5f5e01600607ba06f2
Anthony Luis Acosta <anthonyluislirios.acosta@bicol-u.edu.ph>
2022-08-04 10:08:18 +0800

    Nav links & Application View Update

--- resources/views/admin/application/show.blade.php
+++ resources/views/admin/application/show.blade.php
@@ -1,45 +1,72 @@
 <x-app-layout>
-<x-slot name="header">
+    <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Applications') }} | View
         </h2>
-</x-slot>
+    </x-slot>
 
-<div class="card">
-    <div class="card-header">
-        <div class="row">
-            <div class="col col-md-6"><b>Application Details</b></div>
-            <div class="col col-md-6">
-                <a href="{{ route('admin.application.index') }}" class="btn btn-primary btn-sm float-end">View All</a>
-            </div>
+    <!-- This example requires Tailwind CSS v2.0+ -->
+
+    <div class="block mb-8">
+        <a href="{{ route('admin.application.index') }}" class="bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">Back to list</a>
+    </div>
+    <div class="bg-white shadow overflow-hidden sm:rounded-lg max-w-6xl p-">
+
+        <div class="px-4 py-5 sm:px-6">
+            <h3 class="text-lg leading-6 font-medium text-gray-900">Applicant Information</h3>
+            <p class="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
+        </div>
+        <div class="border-t border-gray-200">
+            <dl>
+                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">Full name</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $application->firstName. "  " .$application->lastName}}</dd>
+                </div>
+                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">Email address</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $application->email }}</dd>
+                </div>
+                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">Gender</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $application->gender }}</dd>
+                </div>
+                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">About</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.</dd>
+                </div>
+                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">Attachments</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        <ul role="list" class="border border-gray-200 rounded-md divide-y divide-gray-200">
+                            <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                                <div class="w-0 flex-1 flex items-center">
+                                    <!-- Heroicon name: solid/paper-clip -->
+                                    <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
+                                    </svg>
+                                    <span class="ml-2 flex-1 w-0 truncate"> Birth Certificate.pdf </span>
+                                </div>
+                                <div class="ml-4 flex-shrink-0">
+                                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Download </a>
+                                </div>
+                            </li>
+                            <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                                <div class="w-0 flex-1 flex items-center">
+                                    <!-- Heroicon name: solid/paper-clip -->
+                                    <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
+                                    </svg>
+                                    <span class="ml-2 flex-1 w-0 truncate"> COR.pdf </span>
+                                </div>
+                                <div class="ml-4 flex-shrink-0">
+                                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Download </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </dd>
+                </div>
+            </dl>
         </div>
     </div>
-    <div class="card-body">
-        <div class="row mb-3">
-            <label class="col-sm-2 col-label-form"><b>Application Name</b></label>
-            <div class="col-sm-10">
-                {{ $application->lastName}}
-            </div>
-        </div>
-        <div class="row mb-3">
-            <label class="col-sm-2 col-label-form"><b>Application Email</b></label>
-            <div class="col-sm-10">
-                {{ $application->email }}
-            </div>
-        </div>
-        <div class="row mb-4">
-            <label class="col-sm-2 col-label-form"><b>Application Gender</b></label>
-            <div class="col-sm-10">
-                {{ $application->gender }}
-            </div>
-        </div>
-        <div class="row mb-4">
-            <label class="col-sm-2 col-label-form"><b>Application Image</b></label>
-            <div class="col-sm-10">
-                <img src="{{ asset('images/' .  $application->applicantImage) }}" width="200" class="img-thumbnail" />
-            </div>
-        </div>
-    </div>
-</div>
 
 </x-app-layout>
