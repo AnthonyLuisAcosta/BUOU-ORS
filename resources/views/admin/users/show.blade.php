5cee97aaf0555d568a6454731b467b1e7c239b92
reizucodes <blaisebueno@gmail.com>
2022-08-06 22:28:32 +0800

    update auth pages

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -9,22 +9,22 @@
       <div class="block mb-8">
         <a href="{{ route('admin.users.index') }}" class="ml-1 inline-flex items-center px-4 py-2 bg-gray-300 border border-transparent rounded-md font-semibold text-xs text-black uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-200 focus:shadow-outline-gray hover:text-white disabled:opacity-25 transition ease-in-out duration-150">Back to list</a>
       </div>
-      <div class="pl-32 max-w-7xl mt-5 sm:col-span-2">
-        <div class=" shadow overflow-hidden rounded-lg">
+      <div class="flex items-center justify-center mt-5">
+        <div class="w-2/3 shadow overflow-hidden rounded-lg">
           <div class="px-4 py-5 bg-white sm:p-6 border-0">
             <!--GRID-->
             <div class="grid grid-rows-flow gap-2">
               <!--First Name-->
               <div class="flex">
-                <p class="text-md p-2 pl-2 w-1/3 text-right border-r-2 mr-2">Name</p>
+                <p class="text-md text-gray-500 p-2 pl-2 w-1/3 text-right border-r-2 mr-2">Name</p>
                 <p class="text-md p-2 w-2/3 block rounded-md bg-gray-100">{{$user->first_name.' '.$user->last_name}}</p>
               </div>
               <div class=" mt-3 flex">
-                <p class="text-md p-2 pl-2 w-1/3 text-right border-r-2 mr-2">Email</p>
+                <p class="text-md text-gray-500 p-2 pl-2 w-1/3 text-right border-r-2 mr-2">Email</p>
                 <p class="text-md p-2 w-2/3 block rounded-md bg-gray-100">{{$user->email}}</p>
               </div>
               <div class=" mt-3 flex">
-                <p class="text-md p-2 pl-2 w-1/3 text-right border-r-2 mr-2">Account Type</p>
+                <p class="text-md text-gray-500 p-2 pl-2 w-1/3 text-right border-r-2 mr-2">Account Type</p>
                 @foreach($roles as $role)
                 @if($user->role_id == $role->role_id)
                 <p class="text-md p-2 w-2/3 block rounded-md bg-gray-100">{{$role->role_name}}</p>
@@ -34,7 +34,6 @@
             </div>
           </div>
           <!--END OF GRID-->
-          <!--Button-->
         </div>
       </div>
     </div>
--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,5 +1,5 @@
 <a href="/">
-    <svg class="w-16 h-16" viewbox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <img src="{{ url('/img/buou.png') }}" class="w-60" />
-    </svg>
+  <svg class="w-16 h-16" viewbox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <img src="{{ url('/img/buou.png') }}" class="w-60 rounded-lg" />
+  </svg>
 </a>
