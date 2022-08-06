4ec9b004c4922ff753cf34908242f3218786fec6
Anthony Luis Acosta <anthonyluislirios.acosta@bicol-u.edu.ph>
2022-08-07 00:14:21 +0800

    Requirements Upload Update

--- resources/views/admin/application/create.blade.php
+++ resources/views/admin/application/create.blade.php
@@ -53,7 +53,7 @@
 							<div class="mt-10 col-span-6 border-t-2 border-gray-200"></div>
 
 							<div class="grid grid-cols-6  gap-4 pb-10">
-								
+
 								<!--Birthdate-->
 								<div class="mt-4 col-span-2">
 									<x-jet-label for="birthDate" value="{{ __('Birth date') }}" />
@@ -69,96 +69,111 @@
 
 									</select>
 								</div>
-										<div class="mt-10 col-span-6 border-t-2 border-gray-200"></div>
-										<label class="font-medium mb-1 text-gray-600 pt-4 block col-span-6">Contact</label>
-									
-										<!--Email-->
-										<div class="mt-4 col-span-4">
-											<x-jet-label for="email" value="{{ __('Email Address') }}" />
-											<x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="email" />
-										</div>
-										<!--Phone-->
-										<div class="mt-4 col-span-3">
-											<x-jet-label for="phone" value="{{ __('Contact Number') }}" />
-											<x-jet-input id="phone" class="block mt-1 w-full" type="number" name="phone" :value="old('phone')" required autofocus autocomplete="phone" />
-										</div>
+								<div class="mt-10 col-span-6 border-t-2 border-gray-200"></div>
+								<label class="font-medium mb-1 text-gray-600 pt-4 block col-span-6">Contact</label>
 
-										<div class="mt-10 col-span-6 border-t-2 border-gray-200"></div>
-										<label class="font-medium mb-1 text-gray-600 pt-4 block col-span-6">Residence Address</label>
+								<!--Email-->
+								<div class="mt-4 col-span-4">
+									<x-jet-label for="email" value="{{ __('Email Address') }}" />
+									<x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="email" />
+								</div>
+								<!--Phone-->
+								<div class="mt-4 col-span-3">
+									<x-jet-label for="phone" value="{{ __('Contact Number') }}" />
+									<x-jet-input id="phone" class="block mt-1 w-full" type="number" name="phone" :value="old('phone')" required autofocus autocomplete="phone" />
+								</div>
 
-										<!--Address-->
-										<div class="mt-4 col-span-6">
-											<x-jet-label for="address" value="{{ __('Address') }}" />
-											<x-jet-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required autofocus autocomplete="address" />
-										</div>
-										<!--Company-->
-										<div class="mt-4 col-span-3">
-											<x-jet-label for="company" value="{{ __('Company') }}" />
-											<x-jet-input id="company" class="block mt-1 w-full" type="text" name="company" :value="old('company')" required autofocus autocomplete="company" />
-										</div>
-										
+								<div class="mt-10 col-span-6 border-t-2 border-gray-200"></div>
+								<label class="font-medium mb-1 text-gray-600 pt-4 block col-span-6">Residence Address</label>
+
+								<!--Address-->
+								<div class="mt-4 col-span-6">
+									<x-jet-label for="address" value="{{ __('Address') }}" />
+									<x-jet-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required autofocus autocomplete="address" />
+								</div>
+								<!--Company-->
+								<div class="mt-4 col-span-3">
+									<x-jet-label for="company" value="{{ __('Company') }}" />
+									<x-jet-input id="company" class="block mt-1 w-full" type="text" name="company" :value="old('company')" required autofocus autocomplete="company" />
+								</div>
+
 							</div>
-						
-				
-									<label class="py-4 pt-10 font-bold mb-1 text-gray-700 block border-b-2 border-gray-200">Upload Requirements</label>
 
-									<div class="mt-10">
-										
-										<div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-											<div class="space-y-1 text-center ">
-												<svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-													<path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-												</svg>
-												<div class="flex text-sm text-gray-600">
-													<label for="applicantImage" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-														<span>Upload a file</span>
-														<input id="applicantImage" name="applicantImage" type="file" required>
-													</label>
-													<p class="pl-1">or drag and drop</p>
-												</div>
-												<p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-											</div>
+
+							<label class="py-4 pt-10 font-bold mb-1 text-gray-700 block border-b-2 border-gray-200">Upload Requirements</label>
+
+							<div class="mt-10">
+
+								<div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+									<div class="space-y-1 text-center ">
+										<svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+											<path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+										</svg>
+										<div class="mx-auto justify-center text-gray-600">
+											<label for="applicantImage" class="cursor-pointer bg-white rounded-md font-bold text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+											</label>
+											<!-- actual upload which is hidden -->
+											<input type="file" id="applicantImage" name="applicantImage" hidden />
+
+											<!-- our custom upload button -->
+											<label for="applicantImage" class="cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">Choose File</label>
+											
 										</div>
+										<!--Display File Name-->
+										<span id="file-chosen" class=" mx-auto justify-center font-sm text-gray-600"></span>
+
+										<p class="text-xs text-gray-500">PNG, JPG, GIF, PDF up to 10MB</p>
 									</div>
-							
-								
+								</div>
+							</div>
+
+
 
 
 							<div class="grid grid-cols-6  gap-4 pt-10">
-								
+
 								<!--Subject Selection-->
 								<div class="mt-4 col-span-6">
 									<label class="pb-4 font-bold mb-1 text-gray-700 block border-b-2 border-gray-200">Subject Selection</label>
 
-									<x-jet-label for="" value="" class="pt-6"/>
+									<x-jet-label for="" value="" class="pt-6" />
 									<select name="" class="form-control block mt-1 w-full text-gray-500 bg-white border-solid border-gray-300 rounded-md">
 										<option selected>Select Subject</option>
+
 										<option class="block mt-1 w-full" value="">Subject 1</option>
 										<option class="block mt-1 w-full" value="">Subject 2</option>
 
 									</select>
 								</div>
-								
 
-								
+
+
 							</div>
-								<!--Button-->
-								<div class="flex items-center justify-end px-4 bg-white text-right sm:px-6 py-6">
-									<input type="submit" value="Add" class="m-6 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
-									
-									
-									<button class="inline-flex items-center px-4 py-2 bg-gray-300 border border-transparent rounded-md font-semibold text-xs text-black uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-200 focus:shadow-outline-gray hover:text-white disabled:opacity-25 transition ease-in-out duration-150">
-										<a href="{{ route('admin.application.index') }}">Cancel</a>
-									</button>
-								</div>
+							<!--Button-->
+							<div class="flex items-center justify-end px-4 bg-white text-right sm:px-6 py-6">
+								<input type="submit" value="Add" class="m-6 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+
+
+								<button class="inline-flex items-center px-4 py-2 bg-gray-300 border border-transparent rounded-md font-semibold text-xs text-black uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-200 focus:shadow-outline-gray hover:text-white disabled:opacity-25 transition ease-in-out duration-150">
+									<a href="{{ route('admin.application.index') }}">Cancel</a>
+								</button>
 							</div>
-							<!--END OF GRID-->
 						</div>
+						<!--END OF GRID-->
+					</div>
 				</form>
 			</div>
 		</div>
 	</div>
 
-	 
+	<script>
+		const applicantImage = document.getElementById('applicantImage');
+
+		const fileChosen = document.getElementById('file-chosen');
+
+		applicantImage.addEventListener('change', function() {
+			fileChosen.textContent = this.files[0].name
+		})
+	</script>
 
 </x-app-layout>
