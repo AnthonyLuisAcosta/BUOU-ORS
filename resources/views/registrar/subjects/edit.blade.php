3cea8ba1f412cdbb45fdb972d6c651ba33cb245a
daniellapena <mariadaniellacaquilala.pena@bicol-u.edu.ph>
2022-08-25 16:18:49 +0800

    updated buttons

--- resources/views/registrar/subjects/edit.blade.php
+++ resources/views/registrar/subjects/edit.blade.php
@@ -10,7 +10,7 @@
             
             <!-- Return Button -->
             <div class="block mb-8">
-              <a href="{{ route('registrar.subjects.index') }}" class="ml-1 inline-flex items-center px-4 py-1 bg-gray-300 border border-transparent rounded-md font-semibold text-xs text-black uppercase tracking-widest hover:bg-gray-500 active:bg-gray-900 focus:outline-none focus:border-gray-200 focus:shadow-outline-gray hover:text-white disabled:opacity-25 transition ease-in-out duration-150">
+              <a href="{{ route('registrar.subjects.index') }}" class="ml-1 inline-flex items-center px-4 py-1 border border-transparent rounded-md font-semibold text-xs text-black uppercase tracking-widest text-gray-800 shadow-md bg-sky-200 hover:bg-sky-400 hover:text-gray-200 disabled:opacity-25 transition ease-in-out duration-150">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M11 17l-5-5m0 0l5-5m-5 5h12" />
                 </svg>
@@ -98,27 +98,14 @@
                                 @endforeach
                             </select>
                           </div>
-        
-                            <!--Enable/disable-->
-                            <!--<div class="mt-4">
-                                <label for="is_enabled" class="inline-flex relative items-center cursor-pointer">
-                                    <input type="checkbox" value="boolean" id="is_enabled" name="is_enabled" class="sr-only peer">
-                                    <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-indigo-300 dark:peer-focus:ring-indigo-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                                    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Is enabled</span>
-                                </label>
-                            </div>-->
                         </div>
                       </div>
                       <!--END OF GRID-->
                       <!--Button-->
                       <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6">
-                        <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                        <button class="inline-flex items-center px-4 py-2  border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest text-gray-800 shadow-md bg-green-200 hover:bg-green-400 hover:text-gray-200 disabled:opacity-25 transition ease-in-out duration-150">
                           Save Changes
                         </button>
-                        <!--<button class="ml-1 inline-flex items-center px-4 py-2 bg-gray-300 border border-transparent rounded-md font-semibold text-xs text-black uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-200 focus:shadow-outline-gray hover:text-white disabled:opacity-25 transition ease-in-out duration-150">
-                          <a href="{{ route('registrar.subjects.create') }}">Create & add</a>
-                        </button>-->
-                        
                       </div>
                     </div>
                 </form>
