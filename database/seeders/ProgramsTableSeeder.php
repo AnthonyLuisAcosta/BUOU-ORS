cb043f66a27a1d5dff43fa05575c5996822f32c1
daniellapena <mariadaniellacaquilala.pena@bicol-u.edu.ph>
2022-11-12 22:11:47 +0800

    added programs seeder

--- database/seeders/ProgramsTableSeeder.php
+++ database/seeders/ProgramsTableSeeder.php
@@ -48,18 +48,24 @@ class ProgramsTableSeeder extends Seeder
             'updated_at' => date("Y-m-d H:i:s"),
         ]);
 
-       /* DB::table('programs')->insert([
+       DB::table('programs')->insert([
             'id' => '4',
             'code' => 'MAELM',
             'description' => 'Master in Education Leadership Management',
+            'adviser' => '3',
+            'dean' => '3',
+            'registrar' => '2',
             'created_at'  => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
         ]);
 
-        /*DB::table('programs')->insert([
+        DB::table('programs')->insert([
             'id' => '5',
             'code' => 'MAEngEd',
             'description' => 'Master of Arts in English Education',
+            'adviser' => '4',
+            'dean' => '3',
+            'registrar' => '2',
             'created_at'  => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
         ]);
@@ -68,6 +74,9 @@ class ProgramsTableSeeder extends Seeder
             'id' => '6',
             'code' => 'MASocStEd',
             'description' => 'Master of Arts in Social Studies Education',
+            'adviser' => '5',
+            'dean' => '3',
+            'registrar' => '2',
             'created_at'  => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
         ]);
@@ -76,6 +85,9 @@ class ProgramsTableSeeder extends Seeder
             'id' => '7',
             'code' => 'EdDELM',
             'description' => 'Doctor of Education in Educational Leadership and Management',
+            'adviser' => '6',
+            'dean' => '3',
+            'registrar' => '2',
             'created_at'  => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
         ]);
@@ -84,8 +96,11 @@ class ProgramsTableSeeder extends Seeder
             'id' => '8',
             'code' => 'BSDevCom',
             'description' => 'Bachelor of Science in Development Communication',
+            'adviser' => '3',
+            'dean' => '3',
+            'registrar' => '2',
             'created_at'  => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
-        ]);*/
+        ]);
     }
 }
